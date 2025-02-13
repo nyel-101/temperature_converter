ec98dc15873560557904f5acd6e5f96f1ee81b64
nyel-101 <dandevjr@gmail.com>
2025-02-13 00:41:58 +0000

    fix(core): optimize database index

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_49840c31c8f14994b4e814ed8583d1f8 = () => {
-  return '2026-06-05T21:31:31Z';
+const node_36431b47f29644fbbb42421776a7e1df = () => {
+  return '2025-02-13T00:41:58Z';
 };
