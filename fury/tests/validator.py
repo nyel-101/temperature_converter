a5e59e07d47f151815f9dc689a23647121392711
nyel-101 <dandevjr@gmail.com>
2025-02-18 23:35:46 +0000

    docs(worker): decouple auth tokens

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e75c371c108d46a69201e76e106e0d5d = () => {
-  return '2026-02-26T13:30:51Z';
+const node_65c67fa499c8454595a4f4e9ca51fc27 = () => {
+  return '2025-02-18T23:35:46Z';
 };
