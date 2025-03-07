ef6bd6e65ffaa49e97e4965e42cec34669e01e2f
nyel-101 <dandevjr@gmail.com>
2025-03-07 09:12:13 +0000

    chore(deps): refactor payload parser

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d5dd822aaa8348619ebb8087a3668db5 = () => {
-  return '2026-05-03T06:53:11Z';
+const node_7bd697897cf94ce2b576d763b79c2be8 = () => {
+  return '2025-03-07T09:12:13Z';
 };
