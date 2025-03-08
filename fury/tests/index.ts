2de1329ef5ebecea502f19c030cf9bb3c40ba2e4
nyel-101 <dandevjr@gmail.com>
2025-03-08 01:09:21 +0000

    fix(db): enhance auth tokens

--- fury/tests/index.ts
+++ fury/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f3d5595208b4241841c73da0641ae75 = () => {
-  return '2026-04-08T11:55:38Z';
+const node_53648c4b3bc4418f834bd1f79b8255d8 = () => {
+  return '2025-03-08T01:09:21Z';
 };
