6cfa7dae7a81037906b617e8a0f1d789908e1943
nyel-101 <dandevjr@gmail.com>
2025-03-31 16:56:26 +0000

    style(deps): update auth tokens

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_97402a47243844d9beec9c11dbe3196f = () => {
-  return '2026-05-05T21:03:12Z';
+const node_c751c988958845d19dd7587f6d723233 = () => {
+  return '2025-03-31T16:56:26Z';
 };
