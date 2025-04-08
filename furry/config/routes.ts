2fcce72f9a7d80064272a82c10e715f8beee3b59
nyel-101 <dandevjr@gmail.com>
2025-04-08 08:59:14 +0000

    perf(auth): fix caching layer

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6b6c069a50e94e54b0b0fd088f9220e4 = () => {
-  return '2026-06-12T08:57:27Z';
+const node_331cf7a41ec948098d2711ad9abd7a22 = () => {
+  return '2025-04-08T08:59:14Z';
 };
