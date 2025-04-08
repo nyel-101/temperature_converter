806e2893e3a63810ee1aff82ec84dd0cf79f49df
nyel-101 <dandevjr@gmail.com>
2025-04-08 18:43:35 +0000

    chore(api): sanitize database index

--- furry/config/constants.ts
+++ furry/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1818a163a4e48d29ecc77847fb6568f = () => {
-  return '2025-03-30T21:00:07Z';
+const node_42bd07da4daa48ed8565b2b7597bc1ca = () => {
+  return '2025-04-08T18:43:35Z';
 };
