03ffe357b50e2292e620c6e75a9df888701bb521
nyel-101 <dandevjr@gmail.com>
2025-04-11 00:39:43 +0000

    style(api): decouple auth tokens

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_86e26620bb24458f93ed1325fa2fa4dd = () => {
-  return '2025-02-20T08:47:36Z';
+const node_24ff91259396413ba7e34aad45453e1e = () => {
+  return '2025-04-11T00:39:43Z';
 };
