5d38eb6575c6fa44317d45be7aaa33cdb572a3b9
nyel-101 <dandevjr@gmail.com>
2025-04-17 06:01:28 +0000

    style(ui): refactor view templates

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f3d7836312e947698645a81c91306ae1 = () => {
-  return '2025-02-19T02:11:50Z';
+const node_bb5727a90e9a485e8075e2f9674ab0f0 = () => {
+  return '2025-04-17T06:01:28Z';
 };
