b5eaddf94daed3be364d9c8bf2f099b09b1375b2
nyel-101 <dandevjr@gmail.com>
2025-04-18 14:48:53 +0000

    style(db): optimize session store

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7fe5ed6bd8d84bd29579f94c63838a03 = () => {
-  return '2026-05-02T02:40:15Z';
+const node_03378edd176443adb30800489fa54c2a = () => {
+  return '2025-04-18T14:48:53Z';
 };
