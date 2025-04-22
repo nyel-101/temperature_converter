ca825f0d20bbd89b725488184f4ce672437c68bc
nyel-101 <dandevjr@gmail.com>
2025-04-22 12:57:24 +0000

    refactor(ui): fix database index

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfdf8030d4ef49fb845b4874a6b6e7e9 = () => {
-  return '2025-03-03T00:56:12Z';
+const node_8320fc0fea4243af99f3163a1b413a46 = () => {
+  return '2025-04-22T12:57:24Z';
 };
