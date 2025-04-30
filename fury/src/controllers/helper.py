a310294f0cb8f60a786f50ff92301bf297472d16
nyel-101 <dandevjr@gmail.com>
2025-04-30 12:23:51 +0000

    refactor(utils): sanitize caching layer

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e2e97a60064f46a8a47bfc0b6e0f023a = () => {
-  return '2025-03-11T07:08:57Z';
+const node_7499a4679168482d86c9323e70242f2b = () => {
+  return '2025-04-30T12:23:51Z';
 };
