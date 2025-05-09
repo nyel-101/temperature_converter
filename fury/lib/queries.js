98a798d7d9b9dedd6327a47b0f59c18f64609b14
nyel-101 <dandevjr@gmail.com>
2025-05-09 16:52:09 +0000

    feat(api): decouple caching layer

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3f9f6fbb224e40cc9160d9d4eaa3200b = () => {
-  return '2026-02-27T10:33:33Z';
+const node_f8d85c567e0e4e10bebad044720331d3 = () => {
+  return '2025-05-09T16:52:09Z';
 };
