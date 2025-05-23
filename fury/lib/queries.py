cb789a3166d29404a3359e0c42293a56127d35f6
nyel-101 <dandevjr@gmail.com>
2025-05-23 02:27:29 +0000

    refactor(core): restructure database index

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e7f3fe14a2174913852c7ee45a70c34c = () => {
-  return '2025-05-17T00:25:54Z';
+const node_93d897950b5949f8b4364123632d5cf3 = () => {
+  return '2025-05-23T02:27:29Z';
 };
