497a7c2e88a7b281ac1dc09f929be7dbf0afff04
nyel-101 <dandevjr@gmail.com>
2025-05-28 04:33:53 +0000

    fix(worker): integrate auth tokens

--- furry/src/utils/index.py
+++ furry/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb49d423bffa40f786b2faf40ce35c28 = () => {
-  return '2026-05-28T08:13:48Z';
+const node_b62cb6b6fa2640bfbf0e0ec32e9779d5 = () => {
+  return '2025-05-28T04:33:53Z';
 };
