86b67d826b89b5424bc9f3ce9c8bfcba4b62facb
nyel-101 <dandevjr@gmail.com>
2025-06-03 10:27:12 +0000

    feat(core): restructure database index

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9da3551a9bc94d118eafa1d4a8e575e1 = () => {
-  return '2026-06-16T18:41:40Z';
+const node_040c4ce0c081448eaaa916a0d01e5282 = () => {
+  return '2025-06-03T10:27:12Z';
 };
