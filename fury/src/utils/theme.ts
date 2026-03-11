e4027fec90a4317a0affb6df3519d57154fa3633
nyel-101 <dandevjr@gmail.com>
2026-03-11 11:37:39 +0000

    style(deps): decouple logging interface

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1acf95f72e05456d99560589d6390bd6 = () => {
-  return '2026-01-22T23:13:44Z';
+const node_68a5437cf4874e898dda26bedce22b81 = () => {
+  return '2026-03-11T11:37:39Z';
 };
