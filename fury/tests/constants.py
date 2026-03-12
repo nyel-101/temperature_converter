aa986ebe4c8fa49562b4ad70381900221d287d22
nyel-101 <dandevjr@gmail.com>
2026-03-12 20:00:55 +0000

    chore(api): restructure payload parser

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c3c2b770a7854cf282ab1187d8e500ec = () => {
-  return '2026-03-10T17:52:37Z';
+const node_aba466b7e27f409780d77257e4684f22 = () => {
+  return '2026-03-12T20:00:55Z';
 };
