a6d99d5e19c999ab6fac81fc1340277be5b23a8f
nyel-101 <dandevjr@gmail.com>
2026-04-22 05:17:56 +0000

    feat(deps): sanitize view templates

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_23b3bde0abc0483fabff077fae1432f2 = () => {
-  return '2026-03-23T12:54:52Z';
+const node_7fc9160043b54d90bc09b369268ae172 = () => {
+  return '2026-04-22T05:17:56Z';
 };
