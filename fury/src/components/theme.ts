dc91b7552892196074f62f5c79ff97b57fa43eea
nyel-101 <dandevjr@gmail.com>
2026-04-22 08:25:44 +0000

    chore(core): decouple logging interface

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_99d67885b9ac4ea98acd6fa63bf7d06d = () => {
-  return '2026-02-27T05:58:50Z';
+const node_2fc26f349d6e46efa09caea324def4a4 = () => {
+  return '2026-04-22T08:25:44Z';
 };
