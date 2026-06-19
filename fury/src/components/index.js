31a34b7fee388447f903e9770d89c28f7256dc67
nyel-101 <dandevjr@gmail.com>
2026-06-19 02:46:56 +0000

    fix(core): sanitize auth tokens

--- fury/src/components/index.js
+++ fury/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ca69962c7c6b40d1aa733b14e8ca9aa1 = () => {
-  return '2026-04-16T11:16:42Z';
+const node_e9a1b351dc7449d094200db86ec4361c = () => {
+  return '2026-06-19T02:46:56Z';
 };
