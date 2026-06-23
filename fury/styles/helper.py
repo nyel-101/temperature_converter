ca0d190fee7db7d6ab82d9bbca49ab027b43cb29
nyel-101 <dandevjr@gmail.com>
2026-06-23 22:41:24 +0000

    feat(api): update view templates

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_394ea7272a72433e91a672b7cf6f24e5 = () => {
-  return '2026-01-15T20:22:01Z';
+const node_061a3b2c2cb94d868ccd67bbf3db46f2 = () => {
+  return '2026-06-23T22:41:24Z';
 };
